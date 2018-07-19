0533f8338281a7b7d07dfc321848f334bf37599f
wichru <wicher.maciej@gmail.com>
2018-07-19 21:35:07 +0200

    some code ipmrovements

--- homework_2/sieve_of_eratosthenes.rb
+++ homework_2/sieve_of_eratosthenes.rb
@@ -1,20 +1,21 @@
 class Prime
 
-  attr_accessor :n
+  attr_accessor :i, :n
 
-  def initialize(n)
+  def initialize(i, n)
+    @i = i
     @n = n
   end
 
   def primes
-  range = (2..n).to_a
+  range = (i..n).to_a
   range.each {|num| range.delete_if {|e| e > num && e % num == 0} }
   range
   end
 
 end
 
-prime = Prime.new(10)
+prime = Prime.new(1, 10)
 puts "Prime numbers: #{prime.primes}"
 
 # require 'prime'
