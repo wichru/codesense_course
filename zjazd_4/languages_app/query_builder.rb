60aea3e2e7dedebbe37a242032d23813b36cc344
wichru <wicher.maciej@gmail.com>
2018-08-05 13:49:41 +0200

    OOP for run method

---
+++ zjazd_4/languages_app/query_builder.rb
@@ -0,0 +1,28 @@
+require 'pg'
+
+
+class QueryBilder
+
+  def initialize(language, is_official)
+    @language = language
+    @is_official = is_official
+  end
+
+  def run
+    "SELECT SUM(city.population), city.district FROM city
+    JOIN country ON country.code = city.countrycode
+    JOIN countrylanguage ON city.countrycode = countrylanguage.countrycode
+    WHERE countrylanguage.language = '#{@language}' #{official(@is_official)}
+    GROUP BY city.district
+    ORDER BY SUM(city.population) DESC;"
+  end
+
+  private
+  def official(param)
+    if param == "is_official"
+      "AND countrylanguage.isofficial = true"
+    elsif param == "is_not_official"
+      "AND countrylanguage.isofficial = false"
+    end
+  end
+end
