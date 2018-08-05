5b4e23f47c311262073222a2347760917b407cdd
wichru <wicher.maciej@gmail.com>
2018-08-05 14:00:57 +0200

    updates

--- zjazd_4/languages_app/language_reporter.rb
+++ zjazd_4/languages_app/language_reporter.rb
@@ -1,14 +1,17 @@
 require 'pg'
+require 'csv'
+require './query_builder'
 
-conn = PG.connect( dbname: 'Pan_Maciej' )
-conn.exec( "SELECT SUM(city.population), city.district FROM city
-JOIN country ON country.code = city.countrycode
-JOIN countrylanguage ON city.countrycode = countrylanguage.countrycode
-WHERE countrylanguage.language = '#{ARGV}' AND countrylanguage.isofficial = true
-GROUP BY city.district
-ORDER BY SUM(city.population)")
-# do |result|
-#   result.each do |row|
-#     print row
-#   end
-# end
+class Execute
+
+  def initialize
+
+
+  end
+
+  def run
+    QueryBilder.new(ARGV[0], ARGV[1]).run
+  end
+end
+
+Execute.new.run
--- zjazd_4/languages_app/question.sql
+++
@@ -1,7 +0,0 @@
-SELECT SUM(city.population), city.district
-FROM city
-JOIN country ON country.code = city.countrycode
-JOIN countrylanguage ON city.countrycode = countrylanguage.countrycode
-WHERE countrylanguage.language = 'Polish' AND countrylanguage.isofficial = true
-GROUP BY city.district
-ORDER BY SUM(city.population);
