192474479f1b01ff8272e171a4e9529e3641e1f5
wichru <wicher.maciej@gmail.com>
2018-08-11 20:15:22 +0200

    loop update

--- homework_3/ruby/fibo.rb
+++ homework_3/ruby/fibo.rb
@@ -1,9 +1,49 @@
-def fib(n)
+def fibo(n)
+  n = n.to_i
   if n.zero? || n == 1
     n
   else
-    fib(n - 1) + fib(n - 2)
+    fib = [0, 1]
+    (n - 2).times do
+      fib << fib[-1] + fib[-2]
+    end
+    puts fib.join("\n")
   end
 end
 
-puts fib(ARGV)
+fibo(ARGV[0])
+
+
+# class Fibonacci
+#   attr_reader :n
+#
+#   def initialize
+#     @n = n
+#   end
+#
+#   def fibo(n)
+#     n = n.to_i
+#     a = 0
+#     b = 1
+#
+#     n.times do
+#       sth = a
+#       a = b
+#       b = sth + b
+#     end
+#     return a
+#   end
+# end
+#
+# class Run
+#   def execute(n)
+#     n = n.to_i
+#     n.times do
+#       block = Fibonacci.new.fibo(ARGV[0])
+#       puts block
+#     end
+#   end
+#
+# end
+#
+# Run.new.execute(ARGV[0])
