c06c5b1a6225f15be6882182eaf5d45f594d0886
wichru <wicher.maciej@gmail.com>
2018-08-14 00:47:14 +0200

    fibo even numbers added

--- homework_3/ruby/sum_even.rb
+++ homework_3/ruby/sum_even.rb
@@ -1,10 +1,27 @@
+# the simplest way to get sum of even numbers
 def sum_even(n)
   p (1..n).select(&:even?).sum
 end
 
+# Range#step and Enumerable#sum method
 def step_even(n)
-  p (1..n).step(4).sum
+  result = []
+  (1..n).step(1) { |x| result << x if x.even? }
+  puts result.sum
+end
+
+# Fibo even numbers
+def fibo_even(n)
+  fib_only_evens = Enumerator.new do |y|
+    a, b = 0, 1
+    loop do
+      y << a if a.even?
+      a, b = b, a + b
+    end
+  end
+  puts fib_only_evens.first(n).reduce(:+)
 end
 
 sum_even(5)
 step_even(5)
+fibo_even(5)
