a772c5cf2ec6dde97075b37c8b34a7d7c70477e6
wichru <wicher.maciej@gmail.com>
2018-08-16 01:33:20 +0200

    updates

--- homework_3/ruby/pesel.rb
+++ homework_3/ruby/pesel.rb
@@ -1,19 +1,39 @@
+class Pesel
+  attr_accessor :pesel
 
-def run
-  correct(ARGV[0])
+  def initialize(pesel)
+    @pesel = pesel
+  end
+
+  def run
+    message
+  end
+
+  def correct
+    control = '13791379131'.split('')
+    result = (@pesel.to_s.split('').inject(0) do |sum, item|
+      sum + item.to_i * control.shift.to_i
+    end % 10).zero?
+    (result ? 'prawidłowy' : 'nieprawidłowy')
+  end
+
+  def sex
+    array = @pesel.to_s.split('').map(&:to_i)
+    (array[9].even? ? 'kobieta' : 'mężczyzna')
+  end
+
+  def date_of_birth
+    dates = @pesel.to_s.split('').map(&:to_i)
+    p dates[4, 2]
+  end
+
+  def message
+    puts "Pesel #{@pesel} jest #{correct}\nInformacje o peselu:
+    - płeć: #{sex}
+    - data urodzenia: "
+  end
 end
 
-def correct(pesel)
-  control = '13791379131'.split('')
-  result = (pesel.to_s.split('').inject(0) do |sum, item|
-    sum + item.to_i * control.shift.to_i
-  end % 10).zero?
-  puts (result ? "Pesel #{pesel} jest prawidłowy" : "Pesel #{pesel} jest nieprawidłowy")
-  puts "Infomacje o peselu:"
-
-end
-
-def information
-end
-
-run
+Pesel.new(ARGV[0]).date_of_birth
+# pesel.correct
+# pesel.sex
